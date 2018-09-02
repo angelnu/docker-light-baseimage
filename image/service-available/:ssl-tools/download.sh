728920cc12cf0687d41233c18912aa9ca295b0a8
Angel Nunez Mencias <git@angelnu.com>
2018-09-02 23:01:23 +0200

    Do not use hardcoded binary

--- image/service-available/:ssl-tools/download.sh
+++ image/service-available/:ssl-tools/download.sh
@@ -15,17 +15,17 @@ if [ -n "$to_install" ]; then
   LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends $to_install
 fi
 
-LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends openssl jq
+LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends openssl jq golang-cfssl
 
-echo "Download cfssl ..."
-curl -o /usr/sbin/cfssl -SL https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
-chmod 700 /usr/sbin/cfssl
-
-echo "Download cfssljson ..."
-curl -o /usr/sbin/cfssljson -SL https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
-chmod 700 /usr/sbin/cfssljson
-
-echo "Project sources: https://github.com/cloudflare/cfssl"
+# echo "Download cfssl ..."
+# curl -o /usr/sbin/cfssl -SL https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+# chmod 700 /usr/sbin/cfssl
+#
+# echo "Download cfssljson ..."
+# curl -o /usr/sbin/cfssljson -SL https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+# chmod 700 /usr/sbin/cfssljson
+#
+# echo "Project sources: https://github.com/cloudflare/cfssl"
 
 # remove tools installed to download cfssl
 if [ -n "$to_install" ]; then
